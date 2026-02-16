e6c37869cf8eb3859b3c96d09b50ca4ec70d16ad
Hamza Plojovic <hamzaplojovic9@gmail.com>
2026-02-16 16:18:23 +0100

    chore: bump dotenvguard to v0.1.1

--- Formula/dotenvguard.rb
+++ Formula/dotenvguard.rb
@@ -3,8 +3,8 @@ class Dotenvguard < Formula
 
   desc "Catch missing .env variables before they crash production"
   homepage "https://github.com/hamzaplojovic/dotenvguard"
-  url "https://files.pythonhosted.org/packages/dd/c3/91cf8d089fdb51a1c94c39b46ddf45da02b50ffa479edd0602682d714867/dotenvguard-0.1.0.tar.gz"
-  sha256 "4c2b99b75e9b9d12754e0fb0e98560c724c353d14279894ef82a540879ed3949"
+  url "https://files.pythonhosted.org/packages/7d/6f/881a2cfc184646d547cf01ffd3d400ae98546dc03c48bbe89868e07f1420/dotenvguard-0.1.1.tar.gz"
+  sha256 "870a2abeca7080edb73e5604f0ef57c54dfc90476b4f16849cf30657de7b501c"
   license "MIT"
 
   depends_on "python@3.12"

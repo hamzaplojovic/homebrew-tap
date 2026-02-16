86ef3d030b196fd2b21abd18a7312b8ada9ccafd
Hamza Plojovic <hamzaplojovic9@gmail.com>
2026-02-16 16:15:56 +0100

    fix: add virtualenv mixin and use virtualenv_install_with_resources

--- Formula/dotenvguard.rb
+++ Formula/dotenvguard.rb
@@ -1,4 +1,6 @@
 class Dotenvguard < Formula
+  include Language::Python::Virtualenv
+
   desc "Catch missing .env variables before they crash production"
   homepage "https://github.com/hamzaplojovic/dotenvguard"
   url "https://files.pythonhosted.org/packages/dd/c3/91cf8d089fdb51a1c94c39b46ddf45da02b50ffa479edd0602682d714867/dotenvguard-0.1.0.tar.gz"
@@ -8,8 +10,7 @@ class Dotenvguard < Formula
   depends_on "python@3.12"
 
   def install
-    venv = virtualenv_create(libexec, "python3.12")
-    venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
